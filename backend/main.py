9b9d5cbe995b1bf173ae9aee41d85c76de0338e1
igarag <n.arranz.agueda@gmail.com>
2021-12-31 19:26:07 +0100

    :sparkles: Finishing backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
+from model import Todo
 from database import (
     fetch_one_todo,
     fetch_all_todos,
@@ -39,15 +40,33 @@ async def get_todo_by_id(title):
         f"There is no TODO item with this title {title}"
     )
 
-@app.put("/api/todo{id}", response_model=Todo)
-async def put_todo(id, data):
-    return id
+@app.put("/api/todo{title}", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(
+        404,
+        f"There is no TODO item with this title {title}"
+    )
 
-@app.post("/api/todo")
-async def post_todo(id, data):
-    return id
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(
+        400,
+        f"Something went wrong / Bad request"
+    )
 
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return id
+@app.delete("/api/todo{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return "Succesfully deleted todo item!"
+    raise HTTPException(
+        404,
+        f"There is no TODO item with this title {title}"
+    )
 
