41cc1c8a1e73b89cba62b78377275e19e9eec3cb
igarag <n.arranz.agueda@gmail.com>
2022-01-29 23:22:30 +0100

    :card_file_box: Include more methods in database file

--- backend/database.py
+++ backend/database.py
@@ -7,6 +7,9 @@ database = client.TodoList
 collection = database.todo
 
 
+async def already_exist(new_document):
+    return collection.find({'UserIDS': {"$in": new_document}}).count() > 0
+
 async def fetch_one_todo(title):
     document = await collection.find_one({"title": title})
     return document
@@ -19,14 +22,21 @@ async def fetch_all_todos():
     return todos
 
 async def create_todo(todo):
-    document = todo
-    result = await collection.insert_one(document)
-    return document
+    if not already_exist(todo):
+        return await collection.insert_one(todo)
+    return None
 
 async def update_todo(title, desc):
-    await collection.update_one({"title": title}, {"$set": {
-        "description": desc
-    }})
+    await collection.update_one(
+        {
+            "title": title,
+        },
+        {
+            "$set": {
+                "description": desc,
+            }
+        }
+    )
     document = await collection.find_one({"title": title})
     return document
 
